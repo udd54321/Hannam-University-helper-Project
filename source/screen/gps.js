0d762a9b52f3b47667fd2978a8f81e1d9fb8bed5
gwangje0625 <118887444+gwangje0625@users.noreply.github.com>
2024-05-25 02:09:21 +0900

    네비게이션 페이지 변경
    네비게이션 페이지 변경

--- source/screen/gps.js
+++ source/screen/gps.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {View, Text, StyleSheet, Dimensions} from 'react-native';
+import {View, StyleSheet, Dimensions} from 'react-native';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
@@ -8,9 +9,18 @@ const windowHeight = Dimensions.get('window').height;
 const Notice = () => {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={style.container}>
-        <View style={{flex: 1}}>
-          <Text>내비게이션 페이지</Text>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.mapContainer}>
+          <MapView
+            provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+            style={styles.map}
+            initialRegion={{
+              latitude: 36.351248966896,
+              longitude: 127.42278350774,
+              latitudeDelta: 0.015,
+              longitudeDelta: 0.0121,
+            }}
+          />
         </View>
       </SafeAreaView>
     </SafeAreaProvider>
@@ -19,10 +29,16 @@ const Notice = () => {
 
 export default Notice;
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+  mapContainer: {
+    flex: 1,
     width: windowWidth,
     height: windowHeight,
-    backgroundColor: '#f0f8ff',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
